93dff0c14785d2e0319ecf4d20592041ffb0bca6
Avery Lin <xinyan.lin@uwaterloo.ca>
2025-06-21 14:34:42 -0400

    train isnt stuck in rails

--- src/components/Train.js
+++ src/components/Train.js
@@ -7,19 +7,19 @@ const Train = () => {
   return (
     <group>
       {/* locomotive */}
-      <Locomotive position={[0, -0.95, 0]} rotation={[0, -Math.PI / 2, 0]}/>
+      <Locomotive position={[0, -0.65, 0]} rotation={[0, -Math.PI / 2, 0]}/>
       
       {/* connector between locomotive and passenger car */}
-      <TrainConnector position={[0, -0.7, -2.65]} rotation={[0, -Math.PI / 2, 0]}/>
+      <TrainConnector position={[0, -0.4, -2.65]} rotation={[0, -Math.PI / 2, 0]}/>
       
       {/* passenger car - positioned behind the locomotive */}
-      <TrainCar position={[0, -0.95, -5.1]} rotation={[0, -Math.PI / 2, 0]}/>
+      <TrainCar position={[0, -0.65, -5.1]} rotation={[0, -Math.PI / 2, 0]}/>
       
       {/* connector between passenger car and cargo car */}
-      <TrainConnector position={[0, -0.7, -7.55]} rotation={[0, -Math.PI / 2, 0]}/>
+      <TrainConnector position={[0, -0.4, -7.55]} rotation={[0, -Math.PI / 2, 0]}/>
       
       {/* cargo car - positioned behind the passenger car */}
-      <TrainCar position={[0, -0.95, -10]} rotation={[0, -Math.PI / 2, 0]}/>
+      <TrainCar position={[0, -0.65, -10]} rotation={[0, -Math.PI / 2, 0]}/>
     </group>
   );
 };
